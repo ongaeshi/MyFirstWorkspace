3a89f96bc41b519e823cb4b6ed15d280afaab7c9
ongaeshi <ongaeshi0621@gmail.com>
2013-06-19 01:57:05 +0900

    Add draw sample code
    ofSetColor
    ofCircle
    ofRect
    ofTriangle
    ofCurve

--- MyFirstWorkspace/src/testApp.cpp
+++ MyFirstWorkspace/src/testApp.cpp
@@ -1,61 +1,78 @@
 #include "testApp.h"
 
 //--------------------------------------------------------------
-void testApp::setup(){
-
+void testApp::setup()
+{
 }
 
 //--------------------------------------------------------------
-void testApp::update(){
-
+void testApp::update()
+{
 }
 
 //--------------------------------------------------------------
-void testApp::draw(){
+void testApp::draw()
+{
+    ofSetColor(255, 255, 255);
+    ofCircle(200, 300, 60);
 
+    ofSetColor(255, 0, 255);
+    ofCircle(300, 300, 60);
+
+    ofSetColor(0, 255, 255);
+    ofCircle(500, 500, 100);
+
+    ofSetColor(0, 255, 128);
+    ofRect(300, 300, 200, 200);
+
+    ofSetColor(0, 128, 128);
+    ofTriangle(400, 100, 500, 200, 300, 200);
+
+    ofSetColor(255, 128, 128);
+    ofCurve(500, 100, 600, 100, 700, 400, 700, 300);
 }
 
 //--------------------------------------------------------------
-void testApp::keyPressed(int key){
-
+void testApp::keyPressed(int key)
+{
 }
 
 //--------------------------------------------------------------
-void testApp::keyReleased(int key){
-
+void testApp::keyReleased(int key)
+{
 }
 
 //--------------------------------------------------------------
-void testApp::mouseMoved(int x, int y){
-
+void testApp::mouseMoved(int x, int y)
+{
 }
 
 //--------------------------------------------------------------
-void testApp::mouseDragged(int x, int y, int button){
-
+void testApp::mouseDragged(int x, int y, int button)
+{
 }
 
 //--------------------------------------------------------------
-void testApp::mousePressed(int x, int y, int button){
-
+void testApp::mousePressed(int x, int y, int button)
+{
 }
 
 //--------------------------------------------------------------
-void testApp::mouseReleased(int x, int y, int button){
-
+void testApp::mouseReleased(int x, int y, int button)
+{
 }
 
 //--------------------------------------------------------------
-void testApp::windowResized(int w, int h){
-
+void testApp::windowResized(int w, int h)
+{
 }
 
 //--------------------------------------------------------------
-void testApp::gotMessage(ofMessage msg){
-
+void testApp::gotMessage(ofMessage msg)
+{
 }
 
 //--------------------------------------------------------------
-void testApp::dragEvent(ofDragInfo dragInfo){ 
-
+void testApp::dragEvent(ofDragInfo dragInfo)
+{
 }

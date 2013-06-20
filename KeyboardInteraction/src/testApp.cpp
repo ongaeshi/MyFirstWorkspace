b8f49f164a90de9beb0982eac1420173f5aa4802
ongaeshi <ongaeshi0621@gmail.com>
2013-06-21 00:38:50 +0900

    add

--- KeyboardInteraction/src/testApp.cpp
+++ KeyboardInteraction/src/testApp.cpp
@@ -2,6 +2,7 @@
 
 int myCircleX;
 int myCircleY;
+int myCircleRadius=100;
 
 //--------------------------------------------------------------
 void testApp::setup()
@@ -15,78 +16,70 @@ void testApp::setup()
 //--------------------------------------------------------------
 void testApp::update()
 {
-    myCircleX += 4;
-
-    // パックマンエフェクト
-    if (myCircleX > 1024) {
-        myCircleX = -60;
-    }
 }
 
 //--------------------------------------------------------------
 void testApp::draw()
 {
-#if 0
-    // 絵を描く
-    ofSetColor(255, 255, 255);
-    ofCircle(200, 300, 60);
-
-    ofSetColor(255, 0, 255);
-    ofCircle(300, 300, 60);
-
-    ofSetColor(0, 255, 255);
-    ofCircle(500, 500, 100);
-
-    ofSetColor(0, 255, 128);
-    ofRect(300, 300, 200, 200);
-
-    ofSetColor(0, 128, 128);
-    ofTriangle(400, 100, 500, 200, 300, 200);
-
-    ofSetColor(255, 128, 128);
-    ofCurve(500, 100, 600, 100, 700, 400, 700, 300);
-
-    ofSetColor(255, 255, 128);
-    ofLine(0, 0, 400, 400);
-
-#else
-    // 動かす
     ofDrawBitmapString(ofToString(ofGetFrameRate())+"fps", 10, 15);
 
     ofSetColor(255, 0, 255);
-    ofCircle(myCircleX, myCircleY, 60);
-
-#endif
+    ofCircle(myCircleX, myCircleY, myCircleRadius);
 }
 
 //--------------------------------------------------------------
 void testApp::keyPressed(int key)
 {
+    int speed = 8;
+
+    if (key == 356) {
+        myCircleX -= speed;
+    } else if (key == 358) {
+        myCircleX += speed;
+    }
+
+    if (key == 357) {
+        myCircleY -= speed;
+    } else if (key == 359) {
+        myCircleY += speed;
+    }
+    // cout << "keyPressed " << key << endl;
 }
 
 //--------------------------------------------------------------
 void testApp::keyReleased(int key)
 {
+    // cout << "keyReleased " << key << endl;
+    // printf("keyReleased %d\n", key);
 }
 
 //--------------------------------------------------------------
 void testApp::mouseMoved(int x, int y)
 {
+    // cout << "mouseMoved: " << x << ", " << y << endl;
 }
 
 //--------------------------------------------------------------
 void testApp::mouseDragged(int x, int y, int button)
 {
+    // cout << "mouseDragged: " << x << ", " << y << " button: " << button << endl;
 }
 
 //--------------------------------------------------------------
 void testApp::mousePressed(int x, int y, int button)
 {
+    float distance = ofDist(myCircleX, myCircleY, x, y);
+
+    if (distance < myCircleRadius) {
+        myCircleRadius++;
+    }
+    // cout << "mousePressed: " << x << ", " << y << " button: " << button << endl;
 }
 
 //--------------------------------------------------------------
 void testApp::mouseReleased(int x, int y, int button)
 {
+     // cout << "mouseReleased: " << x << ", " << y << " button: " << button << endl;
 }
 
 //--------------------------------------------------------------

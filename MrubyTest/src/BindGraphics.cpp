d2c6305054d345cf805e018e662d13f6d692c732
ongaeshi <ongaeshi0621@gmail.com>
2013-06-23 15:50:21 +0900

    Bind text

--- MrubyTest/src/BindGraphics.cpp
+++ MrubyTest/src/BindGraphics.cpp
@@ -20,6 +20,15 @@ namespace {
         ofSetColor(r, g, b);
         return mrb_nil_value();
     }
+
+    static mrb_value text(mrb_state *mrb, mrb_value self)
+    {
+        const char* str;
+        mrb_float x, y;
+        mrb_get_args(mrb, "zff", &str, &x, &y);
+        ofDrawBitmapString(str, x, y);
+        return mrb_nil_value();
+    }
 }
 
 //--------------------------------------------------------------------------------
@@ -27,6 +36,7 @@ void BindGraphics::Bind(mrb_state* mrb)
 {
     mrb_define_method(mrb, mrb->kernel_module, "circle",    circle,    MRB_ARGS_REQ(3));
     mrb_define_method(mrb, mrb->kernel_module, "set_color", set_color, MRB_ARGS_REQ(3));
+    mrb_define_method(mrb, mrb->kernel_module, "text",      text,      MRB_ARGS_REQ(3));
 }
 
 //EOF

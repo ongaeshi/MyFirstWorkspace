1e89face70a1ad8643ba863e09dc57921c2fd05c
ongaeshi <ongaeshi0621@gmail.com>
2013-07-13 01:30:59 +0900

    Add const variable *_BUTTON

--- MrubyTest/src/Mouse.hpp
+++ MrubyTest/src/Mouse.hpp
@@ -4,6 +4,10 @@
 namespace rubybasic {
 class Mouse {
 public:
+    static const int L_BUTTON = 0;
+    static const int M_BUTTON = 1;
+    static const int R_BUTTON = 2;
+    
     Mouse();
 
     void update(int aX, int aY, int aButton);

549b6f3b95e7392c2dc22cf8bdbaa16fda312193
ongaeshi <ongaeshi0621@gmail.com>
2013-07-13 18:21:14 +0900

    Write Input outline

--- MrubyTest/src/Input.hpp
+++ MrubyTest/src/Input.hpp
@@ -2,19 +2,35 @@
 
 #include "Mouse.hpp"
 
-//--------------------------------------------------------------------------------
+//------------------------------------------------------------
 namespace rubybasic {
 class Input {
 public:
     Input();
 
+    /// Update current value
     void update();
 
+    /// @name Basic pad (stick & 1-4 button)
+    //@{
+    // float x() const;
+    // float y() const;
+    // bool isDown(int aCode) const;
+    // bool isPress(int aCode) const;
+    // bool isRelease(int aCode) const;
+    //@}
+
+    /// @name Mouse
+    //@{
     Mouse& mouse() { return mMouse; }
     const Mouse& mouse() const { return mMouse; }
+    //@}
 
 private:
-    Mouse mMouse;
+    // BasicPad mBasicPad;
+    Mouse    mMouse;
+    // Keyboard mKeyboard;
+    // Pad      mPad;
 };
 
 }
